a4075c6ad6b97f4d0b949e4eb5989b31f559f0a2
CL <chaelin.kim@kaist.ac.kr>
2020-03-13 18:46:24 +0900

    Add code and read file

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # alignJointAxes
-Align the z-axis of all joints in the forward direction
+In general, the fullbody animation data operates assuming that the z-axis of all joints in the T-pose is a forward vector.   
+So, it is necesasry to align the z-axis of all joints in the forward direction.   
+This code is for Maya.
+![Alt text](ResultImage.png)
+
+> ### How to implement the code
+> 1. Open "alignJointAxisZForward.py" in Script Editor
+> 2. Ctrl+Enter for executing the code
+> 3. You can use functions with GUI
+
+> ### How to use the GUI
+> ![Alt text](GUIImageExample.PNG)
+> * #### Display local axis of joints
+>   * Checkbox=True: Displaying / False: No displaying
+> * #### Aligning the z-axis of all joints
+>   * Create a new skeleton by copying the joints according to the hirarchy of the original skeleton.
+>   * Then, apply the position value of the copied skeleton to the original skeleton and set the rotation value of the original skeleton to all 0
+>   * Input
+>     * <b>Root joint</b> of target skeleton   
+>       ※ You can drag and drop the joint object into the text field, but you should delete the path in the case of joint object.  
+>     e.g.) |Hips (X) → Hips (O)
+>     * <b>Skin model</b> applied to the target skeleton   
+>       ※ You can drag and drop the skin model.   
+>       ※ If there are multiple skin models, you can put all models by separating with ',' (no blank)   
+>     e.g.) Body, Hair, Clothes (X) → Body,Hair,Clothes (O)
+>   * Click the "<b>Align joints</b>" button, then you can see that the z-axis of all joints are aligned in forward direction.
+>  * #### Rotating the joint in object mode
+>    * In object mode, you can rotate the joint without modifying the mesh and any other joints(e.g. child joints)
+>    * Input
+>      * <b>Joint</b> that you want to rotate
+>      ※ You can drag and drop the joint object even if there is a path in the name.   
+>      e.g.) |Hips|Spine (O)
+>      * <b>x, y, z rotation value</b> how much you want to rotate the joint
+>    * Click the "<b>Rotate joint</b>" button, then you can see that the selected joint only rotates according to values of rotation without modifying other things.
---
+++ alignJointAxisZForward.py
@@ -0,0 +1,224 @@
+import maya.cmds as cmds
+from functools import partial
+
+# Unbind skin (Not used)
+# unbindKeepHistory=True: Keep history�� ���־��? weight ���� ����Ѵ�?.
+# skinning�� Ǯ���鼭 pose�� �޶��������� �� �Լ��� �������? �ʰ� ����.
+def unbindSkinFunc():
+    cmds.skinCluster('kFBXASC045modelShape', e=True, unbind=True, unbindKeepHistory=True, lw=True)
+    #cmds.joint('LeftLeg', e=1, ch=1, oj='xyz', sao='yup')
+    
+    
+# Bind skin (Not used)
+# bindMethod=2: heat map �������? bind
+def bindSkinFunc():
+    cmds.select('kFBXASC045model', add=True)
+    cmds.select('Hips', add=True)
+    cmds.skinCluster(bindMethod=2)
+
+
+# Set moveJointsMode flag in skinCluster function
+# mjm = True: joints can be moved without modifying the skinning (skining�� �������� �ʰ� joint�� ���� ����)
+# After modifying joints, set the mjm=False for taking the skin out of move joints mode(joint ���� �Ŀ� False�� �����ϱ�)
+def setMoveJointsMode(skinModel, mode):
+    #skinModel = skinModel + "Shape"
+    cmds.skinCluster(skinModel, e=True, mjm=mode)
+    #cmds.skinCluster('kFBXASC045modelShape', e=True, mjm=False)
+
+
+# Create new skeleton
+# parentJoint: current selected joint of the original skeleton
+# rootJoint: the root joint of the original skeleton (aligning�� skeleton�� root joint)
+# copyRootJoint: the root joint of the copied skeleton (�� ���縦 ���� ������ skeleton�� root joint)
+def createChildJoint(parentJoint, rootJoint, copyRootJoint) :
+    cmds.select(rootJoint, hi=True)
+    cmds.select(cmds.ls(parentJoint, sl=1))
+    
+    for child in cmds.listRelatives(c=True):        
+        # copy position of origin joint
+        copyPos = cmds.xform(child, q=True, t=True, ws=True)
+        
+        # select parent joint in copy root
+        cmds.select(copyRootJoint, hi=True)
+        if parentJoint != rootJoint:
+            cmds.select(cmds.ls(parentJoint, sl=1))
+        else:
+            cmds.select(cmds.ls(copyRootJoint, sl=1))
+        
+        # create child joint
+        cmds.joint(n=child, p=copyPos)
+            
+        cmds.select(rootJoint, hi=True)
+        if cmds.listRelatives(cmds.ls(child, sl=1), c=True):
+            createChildJoint(child, rootJoint, copyRootJoint)
+        
+        cmds.select(d=True)
+        cmds.select(rootJoint, hi=True)
+        cmds.select(cmds.ls(parentJoint, sl=1))
+     
+    cmds.select(copyRootJoint)
+
+
+# Show local axis of all joints
+# showAxis: flag for showing
+def showAllJointLocalAxis(showAxis, *args):
+    jointList = cmds.ls(type="joint")
+    for jnt in jointList:
+        cmds.setAttr(jnt + ".displayLocalAxis", showAxis)
+
+
+# Copy transform values of copied skeleton to original skeleton
+# parentJoint: current selected joint of the original skeleton
+# rootJoint: the root joint of the original skeleton (aligning�� skeleton�� root joint)
+# copyRootJoint: the root joint of the copied skeleton (�� ���縦 ���� ������ skeleton�� root joint)
+def copyTransformData(parentJoint, rootJoint, copyRootJoint):
+    cmds.select(rootJoint, hi=True)
+    cmds.select(cmds.ls(parentJoint, sl=True))
+    for child in cmds.listRelatives(c=True):        
+        # copy matrix of source joint
+        cmds.select(copyRootJoint, hi=True)
+        cmds.select(cmds.ls(child, sl=True))
+        copyMat = cmds.xform(q=True, m=True, ws=True)
+        
+        # set the transform matrix of copied joint to target joint
+        cmds.select(rootJoint, hi=True)
+        cmds.select(cmds.ls(child, sl=True))
+        cmds.xform(m=copyMat, ws=True)
+        # set zero rotation to target joint
+        cmds.xform(ro=(0, 0, 0))
+        cmds.setAttr(cmds.ls(child, sl=True)[0] + ".rotateAxis", 0, 0, 0)
+        cmds.setAttr(cmds.ls(child, sl=True)[0] + ".jointOrient", 0, 0, 0)        
+        
+        if cmds.listRelatives(cmds.ls(child, sl=1), c=True):
+            copyTransformData(child, rootJoint, copyRootJoint)
+
+
+# Delete the skeleton
+def deleteSkeleton(rootJoint):
+    cmds.select(rootJoint, hi=True)
+    cmds.delete()
+
+# Aligning joint function for UI
+def alignJointsRotAxis(rootJoint, skinModelList, *args):
+    # Get root joint and skin model
+    rootJoint = cmds.textField(rootJoint, q=True, tx=True)
+    skinModelList = cmds.textField(skinModelList, q=True, tx=True)
+    
+    # Check inputs
+    if not rootJoint:
+        print "*Input the root joint"
+        return
+    if not skinModelList:
+        print "*Input the skin model"
+        return
+        
+    skinModelList = skinModelList.split(',')
+    
+    # Aligning process
+    cmds.select(d=True)
+    # Copy root joint
+    rootPos = cmds.xform(rootJoint, q=True, t=True, ws=True)
+    copyRoot = cmds.joint(n=rootJoint, p=rootPos)
+    copyRootName = cmds.ls(copyRoot)[0]
+    
+    # Call createChildJoint
+    root = cmds.ls(rootJoint)[0]
+    createChildJoint(root, rootJoint, copyRootName)
+    
+    print "=== Copying the skeleton is done ==="
+    
+    # Copy source skeleton transformation to target skeleton
+    for skinModel in skinModelList:
+        setMoveJointsMode(skinModel, True)
+    cmds.xform(rootJoint, m=cmds.xform(copyRootName, q=True, m=True, ws=True), ws=True)
+    cmds.xform(rootJoint, ro=(0, 0, 0))
+    cmds.setAttr(rootJoint + '.jointOrient', 0, 0, 0)
+    cmds.setAttr(rootJoint + '.rotateAxis', 0, 0, 0)
+    copyTransformData(root, rootJoint, copyRootName)
+    for skinModel in skinModelList:
+        setMoveJointsMode(skinModel, False)
+    
+    print "=== Aligning the original skeleton is done ==="
+    
+    # delete copied skeleton
+    deleteSkeleton(copyRootName)
+
+
+# Rotate joint function in object mode
+def rotateJoint(selectedJoint, rotX, rotY, rotZ, *args):
+    selectedJoint = cmds.textField(selectedJoint, q=True, tx=True)
+    rotX = cmds.textField(rotX, q=True, tx=True)
+    rotY = cmds.textField(rotY, q=True, tx=True)
+    rotZ = cmds.textField(rotZ, q=True, tx=True)
+    
+    if not selectedJoint:
+        print "*Input the joint name"
+        return
+        
+    if not rotX or not rotY or not rotZ:
+        print "*Input the rotate values"
+        return
+    
+    cmds.selectMode(object=True)
+    cmds.hilite(selectedJoint)
+    cmds.select(selectedJoint + '.rotateAxis', replace=True)
+    cmds.rotate(rotX, rotY, rotZ, relative=True, objectSpace=True, forceOrderXYZ=True)
+
+
+# UI for aligning joint rotation
+def alignmentUI():
+    windowName = "Joint rotation axis controller"
+    windowSize = [310, 330]
+    window = cmds.window(title=windowName, widthHeight=windowSize, sizeable=False)
+
+    cmds.columnLayout("mainLayout")
+    cmds.text(label=" ",h=3)
+    
+    # Display all local joint axis
+    cmds.text(label="Display local axis of all joints", fn="boldLabelFont")
+    cmds.rowLayout("displayAxesLayout", p="mainLayout", numberOfColumns=2, h=35)
+    cmds.text(label="Display local axis   ", p="displayAxesLayout")
+    cmds.checkBox(label="", p="displayAxesLayout",
+                  onCommand=partial(showAllJointLocalAxis, True), offCommand=partial(showAllJointLocalAxis, False))
+    
+    cmds.separator(w=windowSize[0], h=30, p="mainLayout")
+    
+    # Align all joint's z-axis to forward direction
+    cmds.text(label="Align all joint's z-axis to forward direction", fn="boldLabelFont", p="mainLayout")
+    cmds.text(label=" ", h=3, p="mainLayout")
+    # Setting root joint
+    cmds.rowLayout("rootJointLayout", p="mainLayout", numberOfColumns=2)
+    cmds.text(label="Input root joint: ", p="rootJointLayout", w=90, align="left")
+    rootJoint = cmds.textField(tx="Hips", p="rootJointLayout", w=150)
+    # Setting skin model
+    cmds.rowLayout("skinLayout", p="mainLayout", numberOfColumns=2)
+    cmds.text(label="Input skin model: ", p="skinLayout", w=90, align="left")
+    skinModel = cmds.textField(tx="kFBXASC045model", p="skinLayout", w=150)
+    # Aligning button
+    cmds.button(label="Align joints", w=windowSize[0], h=50, p="mainLayout", command=partial(alignJointsRotAxis, rootJoint, skinModel))
+    
+    cmds.separator(w=windowSize[0], h=30, p="mainLayout")
+    
+    # Rotate certain joint
+    cmds.text(label="Rotate the joint in object mode", fn="boldLabelFont", p="mainLayout")
+    cmds.text(label=" ", h=3, p="mainLayout")
+    cmds.rowLayout("selectedJointLayout", p="mainLayout", numberOfColumns=8)
+    # Setting selected joint
+    cmds.text(label="Input joint: ", p="selectedJointLayout")
+    selectedJoint = cmds.textField(p="selectedJointLayout")
+    # Setting rotate values
+    cmds.text(label="x: ", p="selectedJointLayout", w=25, align="right")
+    rotX = cmds.textField(tx="0", p="selectedJointLayout", w=30)
+    cmds.text(label="y: ", p="selectedJointLayout")
+    rotY = cmds.textField(tx="0", p="selectedJointLayout", w=30)
+    cmds.text(label="z: ", p="selectedJointLayout")
+    rotZ = cmds.textField(tx="0", p="selectedJointLayout", w=30)
+    # Rotating button
+    cmds.button(label="Rotate joint", w=windowSize[0], h=50, p="mainLayout", command=partial(rotateJoint, selectedJoint, rotX, rotY, rotZ))
+
+    cmds.showWindow(window)
+    
+
+''' Main '''
+if __name__ == "__main__":
+    alignmentUI()
